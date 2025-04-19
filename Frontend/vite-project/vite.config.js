a657d532b3879a9288a006bd905fa1fdffa257cf
sabapathi <sabapathisiranjeevi@gmail.com>
2025-04-19 09:52:17 +0530

    fix vite build output to dist

--- Frontend/vite-project/vite.config.js
+++ Frontend/vite-project/vite.config.js
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-
-
 export default defineConfig({
   plugins: [react(),
     tailwindcss()
   ],
+  build: {
+    outDir: 'dist',
+   },
   server:{
     port:3000
   }
